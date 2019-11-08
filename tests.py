7950faaddda0f765853758d8ceea30c19bfb498e
Edvin Dunaway <git@eddinn.net>
2019-11-08 13:46:04 +0000

    fixing tests

--- tests.py
+++ tests.py
@@ -57,13 +57,13 @@ class UserModelCase(unittest.TestCase):
 
         # create four posts
         now = datetime.utcnow()
-        p1 = Post(body="post from john", author=u1,
+        p1 = Post(clientname="Test4", clientinfo="post from john", author=u1,
                   timestamp=now + timedelta(seconds=1))
-        p2 = Post(body="post from susan", author=u2,
+        p2 = Post(clientname="Test4", clientinfo="post from susan", author=u2,
                   timestamp=now + timedelta(seconds=4))
-        p3 = Post(body="post from mary", author=u3,
+        p3 = Post(clientname="Test4", clientinfo="post from mary", author=u3,
                   timestamp=now + timedelta(seconds=3))
-        p4 = Post(body="post from david", author=u4,
+        p4 = Post(clientname="Test4", clientinfo="post from david", author=u4,
                   timestamp=now + timedelta(seconds=2))
         db.session.add_all([p1, p2, p3, p4])
         db.session.commit()

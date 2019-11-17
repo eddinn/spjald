62da78f15b1ec29316843422aedb775ec5515fdb
Edvin Dunaway <git@eddinn.net>
2019-11-17 11:57:54 +0000

    _external

--- app/main/routes.py
+++ app/main/routes.py
@@ -22,9 +22,9 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.index', _external=True, page=posts.next_num) \
+    next_url = url_for('main.index', page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('main.index', _external=True, page=posts.prev_num) \
+    prev_url = url_for('main.index', page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('index.html', title='Home',
                            posts=posts.items, next_url=next_url,
@@ -49,9 +49,9 @@ def addpost():
             db.session.add(post)
             db.session.commit()
             flash('Client data successfully added!')
-            return redirect(url_for('main.addpost', _external=True))
+            return redirect(url_for('main.addpost'))
         else:
-            return redirect(url_for('main.index', _external=True))
+            return redirect(url_for('main.index'))
     return render_template('addpost.html', title='Add Post', form=form)
 
 
@@ -65,9 +65,9 @@ def editpost(id):  # pylint: disable=redefined-builtin
             form.populate_obj(qry)
             db.session.commit()
             flash('Your changes have been saved.')
-            return redirect(url_for('main.index', _external=True, id=id))
+            return redirect(url_for('main.index', id=id))
         else:
-            return redirect(url_for('main.index', _external=True))
+            return redirect(url_for('main.index'))
     return render_template('editpost.html', title='Edit post',
                            form=form, id=id)
 
@@ -79,7 +79,7 @@ def deletepost(id):  # pylint: disable=redefined-builtin
     db.session.delete(qry)
     db.session.commit()
     flash('Post successfully deleted!')
-    return redirect(url_for('main.index', _external=True))
+    return redirect(url_for('main.index'))
 
 
 @bp.route('/user/<username>')
@@ -89,10 +89,10 @@ def user(username):
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.user', _external=True, username=user.username,
+    next_url = url_for('main.user', username=user.username,
                        page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('main.user', _external=True, username=user.username,
+    prev_url = url_for('main.user', username=user.username,
                        page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items,
@@ -105,15 +105,14 @@ def follow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         flash('User {} not found.'.format(username))
-        return redirect(url_for('main.index', _external=True))
+        return redirect(url_for('main.index'))
     if user == current_user:
         flash(('You cannot follow yourself!'))
-        return redirect(url_for('main.user', _external=True,
-                                username=username))
+        return redirect(url_for('main.user', username=username))
     current_user.follow(user)
     db.session.commit()
     flash('You are following {}!'.format(username))
-    return redirect(url_for('main.user', _external=True, username=username))
+    return redirect(url_for('main.user', username=username))
 
 
 @bp.route('/unfollow/<username>')
@@ -122,15 +121,14 @@ def unfollow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         flash('User {} not found.'.format(username))
-        return redirect(url_for('main.index', _external=True))
+        return redirect(url_for('main.index'))
     if user == current_user:
         flash('You cannot unfollow yourself!')
-        return redirect(url_for('main.user', _external=True,
-                                username=username))
+        return redirect(url_for('main.user', username=username))
     current_user.unfollow(user)
     db.session.commit()
     flash('You are not following {}.'.format(username))
-    return redirect(url_for('main.user', _external=True, username=username))
+    return redirect(url_for('main.user', username=username))
 
 
 @bp.route('/search')
@@ -141,11 +139,9 @@ def search():
     page = request.args.get('page', 1, type=int)
     posts, total = Post.search(g.search_form.q.data, page,
                                current_app.config['POSTS_PER_PAGE'])
-    next_url = url_for('main.search', _external=True,
-                       q=g.search_form.q.data, page=page + 1) \
+    next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
         if total > page * current_app.config['POSTS_PER_PAGE'] else None
-    prev_url = url_for('main.search', _external=True,
-                       q=g.search_form.q.data, page=page - 1) \
+    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
         if page > 1 else None
     return render_template('search.html', title=('Search'), posts=posts,
                            next_url=next_url, prev_url=prev_url)

5f6b42feb11f5fef4f4968ffa778916a58a25669
Edvin Dunaway <git@eddinn.net>
2021-11-13 19:32:30 +0000

    logging test

--- wsgi.py
+++ wsgi.py
@@ -6,5 +6,5 @@ from spjald import app as application
 
 
 # logging.basicConfig(stream=sys.stderr)
-logging.basicConfig(filename='/var/www/html/fegurdspa/html/spjald/spjald.log', level=logging.ERROR)
+logging.basicConfig(filename='./spjald.log', level=logging.ERROR)
 sys.path.insert(0, "/var/www/html/fegurdspa/html/spjald")

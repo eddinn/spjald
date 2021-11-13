8ec88a7a7f3c7a17950a308819c35251cd1c6f90
Edvin Dunaway <git@eddinn.net>
2021-11-13 19:06:32 +0000

    logging test

--- app/__init__.py
+++ app/__init__.py
@@ -63,18 +63,17 @@ def create_app(config_class=Config):
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
 
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/spjald.log',
-                                           maxBytes=10240, backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s '
-            '[in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Spjald startup')
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/spjald.log',
+                                       maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.ERROR)
+    app.logger.info('Spjald startup')
 
     return app
 
